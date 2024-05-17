0dab719ff1d7d2c97bcae57caeccce27765ac743
Mohit Singh Negi <negi.mohit@flipkart.com>
2024-05-17 19:36:00 +0530

    driver class

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,5 +1,8 @@
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 
 public class Driver {
@@ -10,18 +13,44 @@ public class Driver {
 
         Scanner sc = new Scanner(file);
 
-        while(sc.hasNext()){
+        while (sc.hasNext()) {
 
             String[] cm = sc.nextLine().split(" ");
 
             System.out.println(cm[0]);
 
-            if(cm[0]=="exit"){
+            if (cm[0].equals("exit") ){
                 return;
             }
 
+            if(cm[0] instanceof String){
+
+            }
+            Set<Integer> set1 = new HashSet<Integer>();
+            set1.add(2);
+            set1.remove(3);
+
+            set1.contains(2);
+
+            set1.stream().forEach((Integer integer)->{
+                System.out.printf("consumer interface"+integer);
+            });
+
+            set1.stream().filter((Integer integer)->{
+                return integer==2;
+            }).sorted((Integer i1 , Integer i2)->{
+                return i1<=i2?-1:1;
+            });
+
+//            filter
+//            sorting
         }
-
     }
-
 }
+
+
+
+
+
+
+
